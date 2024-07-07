2c7808b1a2b5b0cd3dc05931cc061e254efdcba8
123dd654 <161431124+123dd654@users.noreply.github.com>
2024-07-08 01:21:31 +0900

    Titanium 제작

---
+++ src/utils/index.js
@@ -0,0 +1,53 @@
+import apple from '/assets/images/apple.svg'
+import search from '/assets/images/search.svg'
+import bag from '/assets/images/bag.svg'
+
+import yellow from '/assets/images/yellow.jpg'
+import white from '/assets/images/white.jpg'
+import blue from '/assets/images/blue.jpg'
+import black from '/assets/images/black.jpg'
+
+import exploremv from "/assets/videos/explore.mp4";
+import explore1 from "/assets/images/explore1.jpg";
+import explore2 from "/assets/images/explore2.jpg";
+
+import hmv from '/assets/videos/hero.mp4'
+import smallmv from '/assets/videos/smallHero.mp4'
+
+import watch from '/assets/images/watch.svg'
+import right from '/assets/images/right.svg'
+
+import pause from '/assets/images/pause.svg'
+import play from '/assets/images/play.svg'
+import replay from '/assets/images/replay.svg'
+
+import highlightsFirstmv from '/assets/videos/highlight-first.mp4'
+import highlightsSecondmv from '/assets/videos/hightlight-sec.mp4'
+import highlightsThirdtmv from '/assets/videos/hightlight-third.mp4'
+import highlightsFourthmv from '/assets/videos/hightlight-fourth.mp4'
+
+export const appleImg = apple;
+export const searchImg = search;
+export const bagImg = bag;
+export const heroVideo = hmv;
+export const smallHeroVideo = smallmv;
+export const watchImg = watch;
+export const rightImg = right;
+export const playImg = play;
+export const replayImg = replay;
+export const pauseImg = pause;
+
+export const highlightsFirstVideo = highlightsFirstmv;
+export const highlightsSecondVideo = highlightsSecondmv;
+export const highlightsThirdtVideo = highlightsThirdtmv;
+export const highlightsFourthVideo = highlightsFourthmv;
+
+export const yellowImg = yellow;
+export const whiteImg = white;
+export const blueImg = blue;
+export const blackImg = black;
+
+export const exploreVideo = exploremv;
+
+export const explore1Img = explore1;
+export const explore2Img = explore2;
